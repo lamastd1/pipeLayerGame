4203eb51357f70967d0d8f769a4238d0255b76fd
Dom Lamastra <dlamastra44@gmail.com>
2023-11-02 22:15:07 -0400

    created a button in pvp controller that can be clicked

--- app/controllers/pvp_controller.rb
+++ app/controllers/pvp_controller.rb
@@ -2,5 +2,9 @@ class PvpController < ApplicationController
 
   def index
   end
+
+  def process_button_click
+    print("yay!")
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'home/about'
   get 'pvp', to:'pvp#index'
 
+  post '/process_button_click', to: 'pvp#process_button_click'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
