2cbf7e7e670695603c6d98839ec4369ea5155e2b
Dom Lamastra <dlamastra44@gmail.com>
2023-11-05 11:10:52 -0500

    added buttons for pipelayer red team

--- app/controllers/pvp_local_controller.rb
+++ app/controllers/pvp_local_controller.rb
@@ -3,14 +3,6 @@ class PvpLocalController < ApplicationController
   def index
   end
 
-  def process_button_click
-    print("yay!")
-  end
-
-  def process_button_click_two
-    print("oh yeahh")
-  end
-
   def process_button
     # print(params[:red_button])
     (1..42).each do |i|
@@ -20,22 +12,11 @@ class PvpLocalController < ApplicationController
       end 
     end
 
-    case params[:blue_button]
-    when 'button_2'
-      print()
-      print()
-      print("BUTTON 2!!")
-      print()
-      print()
-    when 'button_4'
-      print()
-      print()
-      print("BUTTON 4!!")
-      print()
-      print()
+    (43..84).each do |i|
+      if params[:blue_button] == "button_#{i}"
+        print("button_#{i}")
+        print("\n")
+      end 
     end
-  
-    # Redirect or render as needed
   end
-  
 end
