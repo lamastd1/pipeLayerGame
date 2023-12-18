70f6a578f945fb2ecff96db96a3026a6db0b644e
Dom Lamastra <dlamastra44@gmail.com>
2023-12-17 20:41:42 -0500

    added python to ruby on rails project

--- app/controllers/pvp_local_controller.rb
+++ app/controllers/pvp_local_controller.rb
@@ -35,28 +35,34 @@ class PvpLocalController < ApplicationController
 
   def action
 
-    print("HELLO THERE")
-
     # Receive and process the array data here
     used_val = params[:val][7..-1]
-
-    print("USED VAL: \n")
-    print(used_val)
 
     # Perform any necessary actions with the array
     current_move = PvpLocal.create(button_no: used_val, is_visited: false)
 
-    @result = current_move.determine_winner
-    if @result == 1
+    result = `python3 #{Rails.root}/app/python/ai.py`
+    puts "Result from Python script:\n#{result}"
+
+    data_from_python = result.split("\n")
+
+    puts "FIRST ELEMENT"
+    puts data_from_python[0]
+
+    puts "SECOND ELEMENT"
+    puts data_from_python[1]
+
+    @game_result = current_move.determine_winner
+    if @game_result2 == 1
       puts "red wins"
-    elsif @result == 2
+    elsif @game_result2 == 2
       puts "blue wins"
     else
       puts "game is not over"
     end
 
     print("RESULT:")
-    puts @result
+    puts @result2
   
     # Respond to the AJAX request
     respond_to do |format|
